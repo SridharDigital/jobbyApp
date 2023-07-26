597787d1a81f34b86a356e7642abe2a002e765c8
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-07-26 14:29:42 +0530

    updated to single quotes in app.js and index.js files

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import { Switch, Route } from "react-router-dom"
-import "@fontsource/roboto"
+import { Switch, Route } from 'react-router-dom'
+import '@fontsource/roboto'
 
-import Login from "./components/Login"
-import Home from "./components/Home"
-import NotFound from "./components/NotFound"
-import Jobs from "./components/Jobs"
-import jobDetails from "./components/jobDetails"
-import ProtectedRoute from "./components/ProtectedRoute"
+import Login from './components/Login'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import Jobs from './components/Jobs'
+import jobDetails from './components/jobDetails'
+import ProtectedRoute from './components/ProtectedRoute'
 
-import "./App.css"
+import './App.css'
 
 // These are the lists used in the application. You can move them to any component needed.
 const employmentTypesList = [
   {
-    label: "Full Time",
-    employmentTypeId: "FULLTIME",
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
   },
   {
-    label: "Part Time",
-    employmentTypeId: "PARTTIME",
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
   },
   {
-    label: "Freelance",
-    employmentTypeId: "FREELANCE",
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
   },
   {
-    label: "Internship",
-    employmentTypeId: "INTERNSHIP",
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
   },
 ]
 
 const salaryRangesList = [
   {
-    salaryRangeId: "1000000",
-    label: "10 LPA and above",
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
   },
   {
-    salaryRangeId: "2000000",
-    label: "20 LPA and above",
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
   },
   {
-    salaryRangeId: "3000000",
-    label: "30 LPA and above",
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
   },
   {
-    salaryRangeId: "4000000",
-    label: "40 LPA and above",
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
   },
 ]
 
